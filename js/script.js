8769ffed1fe05116fcb42888c4444b9cec2ea960
joniwsfv1 <joniwsfv1@gmail.com>
2026-06-17 21:35:41 -0300

    Novo site com páginas separadas

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-function toggleMenu(){
-
-    const menu = document.getElementById("menu");
-    const btn = document.querySelector(".menu-btn");
-
-    menu.classList.toggle("ativo");
-
-    if(menu.classList.contains("ativo")){
-        btn.innerHTML = "✕";
-    }else{
-        btn.innerHTML = "☰";
-    }
-
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("ativo");
 }
